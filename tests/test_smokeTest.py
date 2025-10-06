7f197921ccc16d39a9d24a07fdf05f5090c65fd5
Israel Carmona <jicmettesc@gmail.com>
2025-10-05 23:52:35 -0600

    Headless update

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -9,15 +9,18 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
-  
+
   def teardown_method(self, method):
     self.driver.quit()
-  
+
   def test_homePage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
@@ -33,14 +36,14 @@ class TestSmokeTest():
     elements = self.driver.find_elements(By.LINK_TEXT, "Join Us")
     assert len(elements) > 0
     self.driver.find_element(By.LINK_TEXT, "Join Us").click()
-  
+
   def test_directoryPage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/directory.html")
     self.driver.find_element(By.ID, "directory-grid").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
     self.driver.find_element(By.ID, "directory-list").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)").text == "Teton Turf and Tree"
-  
+
   def test_joinPage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/join.html")
     elements = self.driver.find_elements(By.NAME, "fname")
@@ -58,7 +61,7 @@ class TestSmokeTest():
     self.driver.find_element(By.NAME, "submit").click()
     elements = self.driver.find_elements(By.NAME, "email")
     assert len(elements) > 0
-  
+
   def test_adminPage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/admin.html")
     elements = self.driver.find_elements(By.ID, "username")
@@ -70,4 +73,4 @@ class TestSmokeTest():
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".errorMessage")
     assert len(elements) > 0
-  
+
